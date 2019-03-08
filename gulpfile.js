d7206479cbf95f1f6db99cc10609c53ff470c7c0
Wen Jen <wjen85@gmail.com>
2019-03-08 12:08:57 -0800

    set up gulp file

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"));
+});
