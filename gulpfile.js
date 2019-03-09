0821d38a052c4480e3d6199db32b56bf363eeac4
Wen Jen <wjen85@gmail.com>
2019-03-09 14:18:51 -0800

    set up image compression and babel in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require('browser-sync').create();
 const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const babel = require("gulp-babel");
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
 
 
 gulp.task("default", ["styles"], function() {
@@ -14,12 +18,15 @@ gulp.task("default", ["styles"], function() {
 
 gulp.task("scripts", function() {
   gulp.src("js/**/*.js")
+    .pipe(babel())
     .pipe(concat("all.js"))
     .pipe(gulp.dest("dist/js"));
 });
 gulp.task("scripts-dist", function() {
   gulp.src("js/**/*.js")
+    .pipe(babel())
     .pipe(concat("all.js"))
+    .pipe(uglify())
     .pipe(gulp.dest("dist/js"));
 });
 
@@ -34,6 +41,13 @@ gulp.task("styles", function() {
     )
     .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
-
 });
 
+gulp.task('images', function() {
+    return gulp.src('img/*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
