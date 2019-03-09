56b33b5376490732ec66fef72e75fee441a5fe1c
Wen Jen <wjen85@gmail.com>
2019-03-09 13:06:17 -0800

    set up concat for css and js in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require('browser-sync').create();
+const concat = require("gulp-concat");
 
 
 gulp.task("default", ["styles"], function() {
@@ -11,16 +12,28 @@ gulp.task("default", ["styles"], function() {
   });
 });
 
+gulp.task("scripts", function() {
+  gulp.src("js/**/*.js")
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest("dist/js"));
+});
+gulp.task("scripts-dist", function() {
+  gulp.src("js/**/*.js")
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest("dist/js"));
+});
+
 gulp.task("styles", function() {
   gulp
     .src("sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
     .pipe(
       autoprefixer({
         browsers: ["last 2 versions"]
       })
     )
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 
 });
+
