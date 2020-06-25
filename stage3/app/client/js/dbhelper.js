1eda981d7c247ddf7c2e0d8a345efcdc4d57ec0e
Wen Jen <wjen1214@gmail.com>
2020-06-24 22:32:22 -0700

    update readme and dbhelper file

--- stage3/app/client/js/dbhelper.js
+++ stage3/app/client/js/dbhelper.js
@@ -1,15 +1,16 @@
 import { openDB, deleteDB, wrap, unwrap } from 'idb';
-import {dbPromise} from '../sw.js';
+import { dbPromise } from '../sw.js';
 
 export default class DBHelper {
-
   static get DATABASE_URL() {
-    const port = 1337 // Change this to your server port
-    return process.env.DATABASE_URL || `https://mwsbackend.herokuapp.com/restaurants`;
+    const port = 1337; // Change this to your server port
+    return (
+      process.env.DATABASE_URL || `https://mwsbackend.herokuapp.com/restaurants`
+    );
   }
 
   static get DATABASE_REVIEWS_URL() {
-    const port = 1337 // Change this to your server port
+    const port = 1337; // Change this to your server port
     return `https://mwsbackend.herokuapp.com/reviews`;
   }
 
@@ -18,20 +19,24 @@ export default class DBHelper {
    */
 
   static fetchRestaurants(callback) {
-    fetch(DBHelper.DATABASE_URL).then(function(response) {
-      response.json().then(function(restaurants) {
+    fetch(DBHelper.DATABASE_URL).then(function (response) {
+      response.json().then(function (restaurants) {
         callback(null, restaurants);
       });
     });
   }
 
-  static fetchReviews( restaurant_id, callback) {
-    let fetchURL = DBHelper.DATABASE_REVIEWS_URL + "/?restaurant_id=" + restaurant_id;
-    fetch(fetchURL).then( response => {
-      return response.json();
-    }).then(reviews => {
+  static fetchReviews(restaurant_id, callback) {
+    let fetchURL =
+      DBHelper.DATABASE_REVIEWS_URL + '/?restaurant_id=' + restaurant_id;
+    fetch(fetchURL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((reviews) => {
         callback(null, reviews);
-      }).catch(error => {
+      })
+      .catch((error) => {
         callback(error, null);
       });
   }
@@ -42,10 +47,12 @@ export default class DBHelper {
       if (error) {
         callback(error, null);
       } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
+        const restaurant = restaurants.find((r) => r.id == id);
+        if (restaurant) {
+          // Got the restaurant
           callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
+        } else {
+          // Restaurant does not exist in the database
           callback('Restaurant does not exist', null);
         }
       }
@@ -62,7 +69,7 @@ export default class DBHelper {
         callback(error, null);
       } else {
         // Filter restaurants to have only given cuisine type
-        const results = restaurants.filter(r => r.cuisine_type == cuisine);
+        const results = restaurants.filter((r) => r.cuisine_type == cuisine);
         callback(null, results);
       }
     });
@@ -78,7 +85,9 @@ export default class DBHelper {
         callback(error, null);
       } else {
         // Filter restaurants to have only given neighborhood
-        const results = restaurants.filter(r => r.neighborhood == neighborhood);
+        const results = restaurants.filter(
+          (r) => r.neighborhood == neighborhood
+        );
         callback(null, results);
       }
     });
@@ -87,18 +96,24 @@ export default class DBHelper {
   /**
    * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
    */
-  static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+  static fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    callback
+  ) {
     // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
-        let results = restaurants
-        if (cuisine != 'all') { // filter by cuisine
-          results = results.filter(r => r.cuisine_type == cuisine);
+        let results = restaurants;
+        if (cuisine != 'all') {
+          // filter by cuisine
+          results = results.filter((r) => r.cuisine_type == cuisine);
         }
-        if (neighborhood != 'all') { // filter by neighborhood
-          results = results.filter(r => r.neighborhood == neighborhood);
+        if (neighborhood != 'all') {
+          // filter by neighborhood
+          results = results.filter((r) => r.neighborhood == neighborhood);
         }
         callback(null, results);
       }
@@ -115,9 +130,13 @@ export default class DBHelper {
         callback(error, null);
       } else {
         // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+        const neighborhoods = restaurants.map(
+          (v, i) => restaurants[i].neighborhood
+        );
         // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+        const uniqueNeighborhoods = neighborhoods.filter(
+          (v, i) => neighborhoods.indexOf(v) == i
+        );
         callback(null, uniqueNeighborhoods);
       }
     });
@@ -133,9 +152,11 @@ export default class DBHelper {
         callback(error, null);
       } else {
         // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
         // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+        const uniqueCuisines = cuisines.filter(
+          (v, i) => cuisines.indexOf(v) == i
+        );
         callback(null, uniqueCuisines);
       }
     });
@@ -145,17 +166,17 @@ export default class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`/restaurant.html?id=${restaurant.id}`);
+    return `/restaurant.html?id=${restaurant.id}`;
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    if(!restaurant.photograph) {
-      return (`/img/${restaurant.id}.jpg`)
+    if (!restaurant.photograph) {
+      return `/img/${restaurant.id}.jpg`;
     }
-    return (`/img/${restaurant.photograph}.jpg`);
+    return `/img/${restaurant.photograph}.jpg`;
   }
 
   /**
@@ -163,49 +184,56 @@ export default class DBHelper {
    */
   static mapMarkerForRestaurant(restaurant, map) {
     // https://leafletjs.com/reference-1.3.0.html#marker
-    const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
-      {title: restaurant.name,
-      alt: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant)
-      })
-      marker.addTo(newMap);
+    const marker = new L.marker(
+      [restaurant.latlng.lat, restaurant.latlng.lng],
+      {
+        title: restaurant.name,
+        alt: restaurant.name,
+        url: DBHelper.urlForRestaurant(restaurant),
+      }
+    );
+    marker.addTo(newMap);
     return marker;
   }
 
   static updateCachedRestaurantReview(formData) {
     console.log('updating cache for new review', formData);
-    return dbPromise.then( db => {
+    return dbPromise.then((db) => {
       const tx = db.transaction('reviews', 'readwrite');
       const store = tx.objectStore('reviews');
       store.put(formData);
       console.log('successfully put review in store');
       return tx.done;
-    })
+    });
   }
 
-/**
-* Grab the original review from the db and replace with edited review
-*/
+  /**
+   * Grab the original review from the db and replace with edited review
+   */
   static editReview(formData, editing) {
-    return dbPromise.then(db => {
-      let tx = db.transaction('reviews');
-      let store = tx.objectStore('reviews');
-      return store.get(editing.id);
-    }).then( review => {
-      return dbPromise.then(db => {
-        let tx = db.transaction('reviews', 'readwrite');
+    return dbPromise
+      .then((db) => {
+        let tx = db.transaction('reviews');
         let store = tx.objectStore('reviews');
-        let newReview = Object.assign({}, review, formData);
-        store.put(newReview);
-        return tx.complete;
+        return store.get(editing.id);
       })
-    });
+      .then((review) => {
+        return dbPromise.then((db) => {
+          let tx = db.transaction('reviews', 'readwrite');
+          let store = tx.objectStore('reviews');
+          let newReview = Object.assign({}, review, formData);
+          store.put(newReview);
+          return tx.complete;
+        });
+      });
   }
 
   static submitReview(formData, editing) {
     console.log(editing);
-    const method = editing ? "PUT" : "POST";
-    const url = editing ? `${DBHelper.DATABASE_REVIEWS_URL}/${editing.id}` : DBHelper.DATABASE_REVIEWS_URL;
+    const method = editing ? 'PUT' : 'POST';
+    const url = editing
+      ? `${DBHelper.DATABASE_REVIEWS_URL}/${editing.id}`
+      : DBHelper.DATABASE_REVIEWS_URL;
     if (editing) {
       DBHelper.editReview(formData, editing);
     } else {
@@ -217,38 +245,45 @@ export default class DBHelper {
   static addPendingRequestToQue(url, method, formData) {
     //open database and add request details to the pending store
     return new Promise((resolve, reject) => {
-      dbPromise.then(db => {
-      const tx = db.transaction('pending', 'readwrite');
-      const store = tx.objectStore('pending');
-      return store.put({
-        data: {
-          url,
-          method,
-          formData
-        }
-      })
-    }).catch(error => {
-      console.log(`Error putting data in pending db: ${error}`);
-    }).then(DBHelper.nextPending((error, json) => {
-      if (error) {
-        console.log(error);
-        return reject(error);
-      }
-      return resolve(json);
-    }));
-  });
+      dbPromise
+        .then((db) => {
+          const tx = db.transaction('pending', 'readwrite');
+          const store = tx.objectStore('pending');
+          return store.put({
+            data: {
+              url,
+              method,
+              formData,
+            },
+          });
+        })
+        .catch((error) => {
+          console.log(`Error putting data in pending db: ${error}`);
+        })
+        .then(
+          DBHelper.nextPending((error, json) => {
+            if (error) {
+              console.log(error);
+              return reject(error);
+            }
+            return resolve(json);
+          })
+        );
+    });
   }
 
   static nextPending(callback) {
-    DBHelper.attemptCommitPending(DBHelper.nextPending).then(j => {
-      console.log(j);
-      callback(null, j);
-    }).catch(error => {
-      console.log(error);
-      if (callback) {
-        callback(error);
-      }
-    });
+    DBHelper.attemptCommitPending(DBHelper.nextPending)
+      .then((j) => {
+        console.log(j);
+        callback(null, j);
+      })
+      .catch((error) => {
+        console.log(error);
+        if (callback) {
+          callback(error);
+        }
+      });
   }
 
   static attemptCommitPending(callback) {
@@ -257,16 +292,16 @@ export default class DBHelper {
     let method;
     let body;
 
-    return new Promise( (resolve, reject) => {
-      dbPromise.then(db => {
+    return new Promise((resolve, reject) => {
+      dbPromise.then((db) => {
         if (!db.objectStoreNames.length) {
-          console.log("DB not available");
+          console.log('DB not available');
           db.close();
           return;
         }
         const tx = db.transaction('pending', 'readwrite');
         const store = tx.objectStore('pending');
-        store.openCursor().then( cursor => {
+        store.openCursor().then((cursor) => {
           if (!cursor) {
             console.log('no more cursors');
             return;
@@ -278,125 +313,139 @@ export default class DBHelper {
 
           // If we don't have a parameter then we're on a bad record that should be tossed
           // and then move on
-          if ((!url || !method) || (method === "POST" && !body)) {
-            cursor
-              .delete()
-              .then(callback);
-              console.log('deleted a bad cursor');
+          if (!url || !method || (method === 'POST' && !body)) {
+            cursor.delete().then(callback);
+            console.log('deleted a bad cursor');
             return;
-          };
+          }
 
           const properties = {
             body: JSON.stringify(body),
-            method: method
-          }
+            method: method,
+          };
 
-          fetch(url, properties).then(response => {
-            console.log(response);
-
-            if (!response.ok && !response.redirected) {
-              console.log('this is the response and we are offline');
+          fetch(url, properties)
+            .then((response) => {
               console.log(response);
-              return;
-            }
-            return response.json().then(json => {
-              const deltx = db.transaction('pending', 'readwrite');
-              const store = deltx.objectStore('pending');
-              return store.openCursor()
-              .then( cursor => {
-                return cursor.delete()
-                .then(() => {
-                  console.log(cursor.value);
-                  console.log('deleted item from pending store');
-                  callback();
-                  console.log(json);
-                  return resolve(json);
+
+              if (!response.ok && !response.redirected) {
+                console.log('this is the response and we are offline');
+                console.log(response);
+                return;
+              }
+              return response.json().then((json) => {
+                const deltx = db.transaction('pending', 'readwrite');
+                const store = deltx.objectStore('pending');
+                return store.openCursor().then((cursor) => {
+                  return cursor.delete().then(() => {
+                    console.log(cursor.value);
+                    console.log('deleted item from pending store');
+                    callback();
+                    console.log(json);
+                    return resolve(json);
+                  });
                 });
               });
+            })
+            .catch((error) => {
+              console.log(error);
+              return reject('no network');
             });
-          }).catch(error => {
-            console.log(error);
-            return reject('no network');
-          });
         });
       });
     });
   }
 
   static syncRestaurant(restaurant) {
-       let url = `https://mwsbackend.herokuapp.com/restaurants/${restaurant.id}/?is_favorite=${restaurant.is_favorite}`;
-       let method = 'PUT';
-       DBHelper.addPendingRequestToQue(url, method).catch(error => {
-         console.log('error updating restaurant backend data...', error, restaurant);
-       });
+    let url = `https://mwsbackend.herokuapp.com/restaurants/${restaurant.id}/?is_favorite=${restaurant.is_favorite}`;
+    let method = 'PUT';
+    DBHelper.addPendingRequestToQue(url, method).catch((error) => {
+      console.log(
+        'error updating restaurant backend data...',
+        error,
+        restaurant
+      );
+    });
   }
 
   static updateRestaurantInDB(new_restaurant) {
-    return dbPromise.then(function(db){
-      let tx = db.transaction('restaurants', 'readwrite');
-      let store = tx.objectStore('restaurants');
-      store.put(new_restaurant);
-      return tx.complete
-    }).then(function(){
-       return Promise.resolve(new_restaurant);
-    });
+    return dbPromise
+      .then(function (db) {
+        let tx = db.transaction('restaurants', 'readwrite');
+        let store = tx.objectStore('restaurants');
+        store.put(new_restaurant);
+        return tx.complete;
+      })
+      .then(function () {
+        return Promise.resolve(new_restaurant);
+      });
   }
 
   static toggleFavBtn(restaurant_id) {
-    return dbPromise.then( db => {
-      let tx = db.transaction('restaurants');
-      let store = tx.objectStore('restaurants');
-      return store.get(restaurant_id);
-    }).then( restaurant => {
-      console.log(restaurant);
-      const new_restaurant = Object.assign({}, restaurant);
-      new_restaurant.is_favorite = (restaurant.is_favorite === 'true' || restaurant.is_favorite === true) ?
-      'false' : 'true';
-      DBHelper.syncRestaurant(new_restaurant);
-      return DBHelper.updateRestaurantInDB(new_restaurant);
-    }).then( new_restaurant => {
-      const favBtn = document.getElementById(`fav-btn-${new_restaurant.id}`);
-      if(new_restaurant.is_favorite === 'true' || new_restaurant.is_favorite === true) {
-        favBtn.innerHTML = 'Favorited!';
-        favBtn.style.background = 'hotpink';
-      } else {
-        favBtn.innerHTML = 'Add to favorite';
-        favBtn.style.background = 'grey';
-      }
-    })
+    return dbPromise
+      .then((db) => {
+        let tx = db.transaction('restaurants');
+        let store = tx.objectStore('restaurants');
+        return store.get(restaurant_id);
+      })
+      .then((restaurant) => {
+        console.log(restaurant);
+        const new_restaurant = Object.assign({}, restaurant);
+        new_restaurant.is_favorite =
+          restaurant.is_favorite === 'true' || restaurant.is_favorite === true
+            ? 'false'
+            : 'true';
+        DBHelper.syncRestaurant(new_restaurant);
+        return DBHelper.updateRestaurantInDB(new_restaurant);
+      })
+      .then((new_restaurant) => {
+        const favBtn = document.getElementById(`fav-btn-${new_restaurant.id}`);
+        if (
+          new_restaurant.is_favorite === 'true' ||
+          new_restaurant.is_favorite === true
+        ) {
+          favBtn.innerHTML = 'Favorited!';
+          favBtn.style.background = 'hotpink';
+        } else {
+          favBtn.innerHTML = 'Add to favorite';
+          favBtn.style.background = 'grey';
+        }
+      });
   }
 
   static deleteCachedReview(review_id) {
-    return dbPromise.then(db => {
-      let tx = db.transaction('reviews', 'readwrite')
-      let store =  tx.objectStore('reviews');
-      store.delete(review_id);
-      console.log('deleted review from idb');
-      return tx.complete;
-    }).catch(error => {
-      console.log('error deleting review: ', error);
-    });
+    return dbPromise
+      .then((db) => {
+        let tx = db.transaction('reviews', 'readwrite');
+        let store = tx.objectStore('reviews');
+        store.delete(review_id);
+        console.log('deleted review from idb');
+        return tx.complete;
+      })
+      .catch((error) => {
+        console.log('error deleting review: ', error);
+      });
   }
 
   static deleteReview(review_id) {
     const url = `${DBHelper.DATABASE_REVIEWS_URL}/${review_id}`;
     console.log(url);
-    const method = "DELETE";
+    const method = 'DELETE';
     DBHelper.deleteCachedReview(review_id);
     return DBHelper.addPendingRequestToQue(url, method);
   }
 
   static deleteTempReview(temp_id) {
-    dbPromise.then(db => {
-      let tx = db.transaction('reviews', 'readwrite')
-      let store = tx.objectStore('reviews');
-      store.delete(temp_id);
-      console.log('deleted oldversion of review with old id');
-      return tx.complete;
-    }).catch( error => {
-      console.log('error deleting temp review: ', error);
-    })
+    dbPromise
+      .then((db) => {
+        let tx = db.transaction('reviews', 'readwrite');
+        let store = tx.objectStore('reviews');
+        store.delete(temp_id);
+        console.log('deleted oldversion of review with old id');
+        return tx.complete;
+      })
+      .catch((error) => {
+        console.log('error deleting temp review: ', error);
+      });
   }
 }
-
-
